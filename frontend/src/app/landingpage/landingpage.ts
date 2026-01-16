6c3665923446d62b092b896c4e06d7e2d340b3a4
Pike <20842719+Pikego@users.noreply.github.com>
2026-01-16 16:37:09 +0100

    Update landing page

--- frontend/src/app/landingpage/landingpage.ts
+++ frontend/src/app/landingpage/landingpage.ts
@@ -1,16 +1,29 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
-import { TuiButton, TuiError, TuiTextfield } from '@taiga-ui/core';
-import { TuiFieldErrorPipe } from '@taiga-ui/kit';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { catchError, finalize, of, tap } from 'rxjs';
+
+import { TuiAppearance, TuiButton, TuiError, TuiTextfield, TuiTitle } from '@taiga-ui/core';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+import { TuiAutoFocus } from '@taiga-ui/cdk';
 
 import { ApiService } from '../common/api.service';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, TuiButton, TuiError, TuiTextfield],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    TuiAppearance,
+    TuiHeader,
+    TuiCardLarge,
+    TuiTitle,
+    TuiButton,
+    TuiError,
+    TuiTextfield,
+    TuiAutoFocus,
+  ],
   templateUrl: './landingpage.html',
   styleUrl: './landingpage.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,9 +31,11 @@ import { ApiService } from '../common/api.service';
 export class Landingpage {
   private readonly apiService = inject(ApiService);
 
-  protected readonly nameControl = new FormControl('', {
-    nonNullable: true,
-    validators: [Validators.required],
+  protected readonly form = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
   });
 
   protected readonly roomId = signal<string | null>(null);
@@ -28,7 +43,7 @@ export class Landingpage {
   protected readonly isLoading = signal(false);
 
   protected createRoom(): void {
-    if (this.nameControl.invalid) {
+    if (this.form.invalid) {
       return;
     }
 
@@ -37,7 +52,7 @@ export class Landingpage {
     this.roomId.set(null);
 
     this.apiService
-      .createRoom(this.nameControl.value)
+      .createRoom(this.form.controls.name.value)
       .pipe(
         tap(({ roomId }) => {
           this.roomId.set(roomId);
